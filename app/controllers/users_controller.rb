2c561e130a189b61b99c863fb69406738680cf94
Ben Baral <bbaral@unify.id>
2020-07-24 15:29:59 -0700

    Fix whitespace

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   skip_before_action :authorized
   skip_before_action :verify_authenticity_token, only: :trust
   http_basic_authenticate_with name: Rails.application.credentials.unifyid[:basic_username],
